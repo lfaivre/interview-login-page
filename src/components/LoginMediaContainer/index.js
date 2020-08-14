c738492972f2099f574e91b5334a66f86dc07b2b
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-08-14 15:13:19 -0700

    feat: add art asset

--- src/components/LoginMediaContainer/index.js
+++ src/components/LoginMediaContainer/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './index.css';
+import LoginPageArt from '../../assets/login_page_art.svg';
 
 const LoginMediaContainer = () => {
-  return <div className="login-media-container"></div>;
+  return (
+    <div className="login-media-container">
+      <img src={LoginPageArt} alt="Placeholder Art" className="art" />
+    </div>
+  );
 };
 
 export default LoginMediaContainer;
