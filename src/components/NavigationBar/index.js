fd448e0ad6c3a0bac1b9a0cc66d0b7a6b05f51e3
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-08-14 11:30:13 -0700

    feat: build out navigation bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './index.css';
+import profileImage from '../../assets/profile.jpeg';
 
 const NavigationBar = () => {
   return (
     <div className="navigation-bar">
       <div className="inner-container">
         <div className="left">
-          <p className="name">Company.</p>
+          <a href="/" className="name">
+            Company.
+          </a>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <ul className="navigation-links">
+            <li className="link">
+              <a href="/">About</a>
+            </li>
+            <li className="link">
+              <a href="/">Services</a>
+            </li>
+            <li className="link">
+              <a href="/">Showcase</a>
+            </li>
+            <li className="link">
+              <a href="/">Account</a>
+            </li>
+          </ul>
+          <div className="account">
+            <div className="picture" style={{ backgroundImage: `url(${profileImage})` }}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
